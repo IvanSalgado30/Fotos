6eeceb6b312a7d44f88903dbcd5ed2bbeed58764
IvanSalgado30 <hola.08.webo@hotmail.com>
2020-12-05 19:23:34 -0500

    commit paty un

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ad inventore accusamus ipsa tempore quibusdam accusantium quod, saepe recusandae iure deserunt, dolore nam quasi architecto vero? Porro culpa sed nemo.
+    </p>
+     <div>
+         <p>
+             monse es buena
+       </p>
+     </div>
+</body>
+</html>

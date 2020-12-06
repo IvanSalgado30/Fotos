ba23409160b5909296722bd01f4e6c138b861354
IvanSalgado30 <hola.08.webo@hotmail.com>
2020-12-05 20:58:52 -0500

    sip

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de sesion</title>
-    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
         
     
     </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
